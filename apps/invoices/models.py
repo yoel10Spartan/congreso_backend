b85a7015c13525f52f2404d0a1fb33e01bc10a3e
Yoel Muñoz Zecua <munozzecuayoel@gmail.com>
2022-05-23 15:41:50 -0500

    Change Model Invoice

--- apps/invoices/models.py
+++ apps/invoices/models.py
@@ -6,7 +6,7 @@ class Invoice(models.Model):
     rfc = models.CharField(max_length=50)
     street = models.CharField(max_length=100)
     outdoor_number = models.IntegerField()
-    interior_number = models.IntegerField(null=True, blank=True)
+    interior_number = models.IntegerField(null=True, blank=True, default=0)
     suburb = models.CharField(max_length=100)
     cp = models.BigIntegerField()
     municipality = models.CharField(max_length=100)
