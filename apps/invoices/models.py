d26fd210385eed76e370422a34d9abe1325da0cb
spartan <spartan@spartan.spartan>
2022-08-04 22:54:51 -0500

    Add more length

--- apps/invoices/models.py
+++ apps/invoices/models.py
@@ -2,19 +2,19 @@ from statistics import mode
 from django.db import models
 
 class Invoice(models.Model):
-    name = models.CharField(max_length=50)
-    rfc = models.CharField(max_length=50)
-    street = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    rfc = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
     outdoor_number = models.IntegerField()
-    interior_number = models.IntegerField(null=True, blank=True, default=0)
-    suburb = models.CharField(max_length=100)
+    interior_number = models.CharField(max_length=255, default='1')
+    suburb = models.CharField(max_length=255)
     cp = models.BigIntegerField()
-    municipality = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
+    municipality = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
     email = models.EmailField()
     cell_phone_number = models.BigIntegerField()
-    way_pay = models.CharField(max_length=100)
-    bill_usage = models.CharField(max_length=250)
+    way_pay = models.CharField(max_length=255)
+    bill_usage = models.CharField(max_length=255)
     
     def __str__(self) -> str:
         return self.name
